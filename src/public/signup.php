4f5e71df1dabd58f3cb93d118bed71b02660f134
Ura20 <ura2091.212@gamail.com>
2024-11-04 14:56:54 +0900

    feat:会員登録フォームの実装

--- src/public/signup.php
+++ src/public/signup.php
@@ -2,9 +2,22 @@
   <div>
     <div>
       <h2>会員登録</h2>
+      <?php foreach ($errors as $error): ?>
+        <p><?php echo $error; ?></p>
+      <?php endforeach; ?>
       <form action="./signup_complete.php" method="POST">
-        <p><input placeholder="User name" type=“text” name="userName" required value=""></p>
-        <p><input placeholder="Email" type=“mail” name="email" required value=""></p>
+        <p>
+          <input placeholder="User name" type=“text” name="userName" required value="<?php if (
+              isset($_SESSION['userName'])
+          ) {
+              echo $_SESSION['userName']; //アカウント作成ボタン押下後、登録失敗時にsignin.phpを表示→その時に入力していた項目をフォームに表示させる
+          } ?>">
+        </p>
+        <p><input placeholder="Email" type=“mail” name="email" required value="<?php if (
+            isset($_SESSION['email'])
+        ) {
+            echo $_SESSION['email'];
+        } ?>"></p>
         <p><input placeholder="Password" type="password" name="password"></p>
         <p><input placeholder="Password確認" type="password" name="confirmPassword"></p>
         <button type="submit">アカウント作成</button>
